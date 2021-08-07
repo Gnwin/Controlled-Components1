ce2aea0fe7b1811cb3d1d0538967b372cb74ae16
Godwin Nwachukwu <nwgwin@gmail.com>
2021-08-07 18:06:06 +0000

    create three subcomponents

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class Button extends from Component {
+  constructor(props){
+    super(props)
+    //this.state = {
+      //value: '',
+      //items: []
+    //}
+  }
+  
+  render(){
+    return()
+  }
+}
+
+export default Button
---
+++ src/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class List extends from Component {
+  constructor(props){
+    super(props)
+    //this.state = {
+      //value: '',
+      //items: []
+    //}
+  }
+  
+  render(){
+    return()
+  }
+}
+
+export default List
